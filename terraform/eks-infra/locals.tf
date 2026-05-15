923cf36899d460380dcc2266afe0cca78e10ecf2
thefloor567 <wodbs1243@naver.com>
2026-05-15 15:23:47 +0900

    --

---
+++ terraform/eks-infra/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  name_prefix = "tf-prac-${var.environment}"
+}
---
+++ terraform/eks-infra/vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "${local.name_prefix}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${local.name_prefix}-igw"
+  }
+}
